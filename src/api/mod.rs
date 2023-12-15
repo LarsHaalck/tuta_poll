1d0c51b82f16aeaa227f09822c67ff96c665ef9f
Lars Haalck <lars.haalck@uni-muenster.de>
2023-12-15 10:51:37 +0100

    update versions

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,7 +1,7 @@
 pub const BASE_URL: &'static str = "https://app.tuta.com";
 pub const CLIENT: &'static str = "Firefox Browser";
-pub const CLIENT_VERSION: &'static str = "3.118.30";
-pub const MODEL_VERSION: &'static str = "90.64";
+pub const CLIENT_VERSION: &'static str = "3.119.6";
+pub const MODEL_VERSION: &'static str = "91.65";
 
 pub mod bucket_permission;
 pub mod group;
